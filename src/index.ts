3e9b699a3d4d0a0db163d5c5bc3d97de42caa6ab
Yan Duarte <yduarte.ctr@touchbistro.com>
2023-08-15 20:29:28 -0300

    Sending data to Event-Bus

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,55 @@
-import express, { Response } from 'express'
-import { json } from 'body-parser'
-import { randomBytes } from 'crypto'
-import { Comments, PostCommentsRequest } from './types'
-import cors from 'cors'
+import express, { Response } from "express";
+import { json } from "body-parser";
+import { randomBytes } from "crypto";
+import { Comments, PostCommentsRequest } from "./types";
+import cors from "cors";
+import axios from "axios";
 
-const app = express()
-app.use(json())
-app.use(cors())
+const app = express();
+app.use(json());
+app.use(cors());
 
-const commentsByPostId: Comments = {}
+const commentsByPostId: Comments = {};
 
-app.get('/posts/:id/comments', (req, res: Response<Comments[0]>) => {
-  const { id: postId } = req.params
-  res.send(commentsByPostId[postId] ?? [])
-})
+app.get("/posts/:id/comments", (req, res: Response<Comments[0]>) => {
+  const { id: postId } = req.params;
+  res.send(commentsByPostId[postId] ?? []);
+});
 
-app.post('/posts/:id/comments', (req: PostCommentsRequest, res: Response<Comments[0]>) => {
-  const commentId = randomBytes(4).toString('hex')
-  const { content } = req.body
-  const { id: postId } = req.params
+app.post(
+  "/posts/:id/comments",
+  async (req: PostCommentsRequest, res: Response<Comments[0]>) => {
+    const commentId = randomBytes(4).toString("hex");
+    const { content } = req.body;
+    const { id: postId } = req.params;
 
-  const comments = [
-    ...commentsByPostId[postId] ?? [],
-    { id: commentId, content }
-  ]
+    const comments = [
+      ...(commentsByPostId[postId] ?? []),
+      { id: commentId, content },
+    ];
 
-  commentsByPostId[postId] = comments
+    commentsByPostId[postId] = comments;
 
-  res.status(201).send(comments)
-})
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentCreated",
+      data: {
+        id: commentId,
+        content,
+        postId,
+      },
+    });
+
+    res.status(201).send(comments);
+  }
+);
+
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
+  console.log("Received Event", { type, data });
+
+  res.send({});
+});
 
 app.listen(4001, () => {
-  console.log('Listening port 4001')
-})
+  console.log("Listening port 4001");
+});
